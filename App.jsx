7ad8a88d2d2cb5d94c1ed3f531205d83e4f67e1f
Hrithik Kedare <kedarehrithik@gmail.com>
2023-08-06 10:28:01 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,643 @@
+import React from "react";
+
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// const heading = React.createElement("h1", { id: "title" }, "Heading 1");
+// const heading2 = React.createElement("h2", { id: "title2" }, "Heading 2");
+// //     React element                  ("html tags","object","values")
+// const container = React.createElement("div", { id: "container" }, [
+//   heading,
+//   heading2,
+// ]);
+
+// If we want to put multiple things inside any element, we use [] "Array"
+
+// index.js:1 Warning: Each child in a list should have a unique "key" prop.
+// We will learn about keys later but for some context it is similar to ids.
+
+// The swiggy clone
+
+/***
+ * Header
+ *  -Logo
+ *  -Nav Items
+ *  -Cart
+ * Body
+ *  -Search Bar
+ *  -Restaurant List
+ *      +Restaurant Cards(Many Cards)
+ *          -Image
+ *          -Name
+ *          -Rating
+ *          -Cuisine
+ * Footer
+ *  -Links
+ */
+
+const Title = () => {
+  return (
+    <a href="/">
+      <img
+        className="logo"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfTxOeXY1KFuFnRxeuRaCw21r8U5kZ2Bm6MA&usqp=CAU"
+      />
+    </a>
+  );
+};
+
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantList = [
+  {
+    info: {
+      id: "480975",
+      name: "Domino's Pizza",
+      cloudinaryImageId: "ffz16mt19jutcrnfxs8a",
+      locality: "SHREERAM PLAZA",
+      areaName: "Kalyan",
+      costForTwo: "₹400 for two",
+      cuisines: ["Pizzas", "Italian", "Pastas", "Desserts"],
+      avgRating: 3.7,
+      feeDetails: {
+        restaurantId: "480975",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 3000,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 3000,
+      },
+      parentId: "2456",
+      avgRatingString: "3.7",
+      totalRatingsString: "100+",
+      sla: {
+        deliveryTime: 25,
+        serviceability: "SERVICEABLE",
+        slaString: "25 mins",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-08-06 22:59:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹150 OFF",
+        subHeader: "ABOVE ₹999",
+        discountTag: "FLAT DEAL",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-c1dacc1c-dcd8-4cd5-b9d5-072491c5bb0b",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/dominos-pizza-shreeram-plaza-kalyan-mumbai-480975",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "466227",
+      name: "Uma kitchen",
+      cloudinaryImageId: "k1juipm5lyzysc7m993k",
+      locality: "Ulhasnagar",
+      areaName: "Kalyan",
+      costForTwo: "₹310 for two",
+      cuisines: ["Beverages"],
+      veg: true,
+      feeDetails: {
+        restaurantId: "466227",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 9700,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 9700,
+      },
+      parentId: "219264",
+      avgRatingString: "--",
+      sla: {
+        deliveryTime: 66,
+        lastMileTravel: 9.2,
+        serviceability: "SERVICEABLE",
+        slaString: "66 mins",
+        lastMileTravelString: "9.2 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-08-06 23:55:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        discountCalloutInfo: {
+          message: "Free Delivery",
+          logoCtx: {
+            logo: "v1655895371/free_delivery_logo_hqipbo.png",
+          },
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-c1dacc1c-dcd8-4cd5-b9d5-072491c5bb0b",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/uma-kitchen-ulhasnagar-kalyan-mumbai-466227",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "75536",
+      name: "McDonald's",
+      cloudinaryImageId: "ee5f8e06b300efc07c9fe3f4df40dfc4",
+      locality: "Khadakpada Circle",
+      areaName: "Kalyan",
+      costForTwo: "₹400 for two",
+      cuisines: ["Burgers", "Beverages", "Cafe", "Desserts"],
+      avgRating: 4.1,
+      feeDetails: {
+        restaurantId: "75536",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 11700,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 11700,
+      },
+      parentId: "630",
+      avgRatingString: "4.1",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 58,
+        lastMileTravel: 11.5,
+        serviceability: "SERVICEABLE_WITH_BANNER",
+        slaString: "58 mins",
+        lastMileTravelString: "11.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextOpenTimeMessage: "Currently not taking orders for this location",
+        nextCloseTime: "2023-08-07 01:45:00",
+        opened: true,
+      },
+      badges: {
+        textExtendedBadges: [
+          {
+            iconId: "guiltfree/GF_Logo_android_3x",
+            shortDescription: "options available",
+            fontColor: "#7E808C",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "",
+                  fontColor: "#7E808C",
+                  iconId: "guiltfree/GF_Logo_android_3x",
+                  shortDescription: "options available",
+                },
+              },
+            ],
+          },
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "10% OFF",
+        subHeader: "ABOVE ₹999",
+        discountTag: "FLAT DEAL",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-c1dacc1c-dcd8-4cd5-b9d5-072491c5bb0b",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/mcdonalds-khadakpada-circle-kalyan-mumbai-75536",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "203926",
+      name: "Burger King",
+      cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+      locality: "Barave Road",
+      areaName: "Kalyan",
+      costForTwo: "₹350 for two",
+      cuisines: ["Burgers", "American"],
+      avgRating: 4.1,
+      feeDetails: {
+        restaurantId: "203926",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 11700,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 11700,
+      },
+      parentId: "166",
+      avgRatingString: "4.1",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 62,
+        lastMileTravel: 11.5,
+        serviceability: "SERVICEABLE_WITH_BANNER",
+        slaString: "62 mins",
+        lastMileTravelString: "11.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextOpenTimeMessage: "Currently not taking orders for this location",
+        nextCloseTime: "2023-08-07 03:45:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "60% OFF",
+        subHeader: "UPTO ₹120",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-c1dacc1c-dcd8-4cd5-b9d5-072491c5bb0b",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/burger-king-barave-road-kalyan-mumbai-203926",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "92242",
+      name: "Pizza Hut",
+      cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      locality: "Kalyan East",
+      areaName: "Kalyan East",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 3.7,
+      feeDetails: {
+        restaurantId: "92242",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 10700,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 10700,
+      },
+      parentId: "721",
+      avgRatingString: "3.7",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 67,
+        lastMileTravel: 10.6,
+        serviceability: "SERVICEABLE_WITH_BANNER",
+        slaString: "67 mins",
+        lastMileTravelString: "10.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextOpenTimeMessage: "Currently not taking orders for this location",
+        nextCloseTime: "2023-08-06 23:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "60% OFF",
+        subHeader: "UPTO ₹120",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-c1dacc1c-dcd8-4cd5-b9d5-072491c5bb0b",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/pizza-hut-kalyan-east-mumbai-92242",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "169878",
+      name: "NIC Ice Creams",
+      cloudinaryImageId: "c8b98c4064dc2fd6bdf350212e5f0bd5",
+      locality: "Pushpakdham",
+      areaName: "Kalyan",
+      costForTwo: "₹120 for two",
+      cuisines: ["Ice Cream", "Desserts"],
+      avgRating: 4.5,
+      veg: true,
+      feeDetails: {
+        restaurantId: "169878",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 11700,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 11700,
+      },
+      parentId: "6249",
+      avgRatingString: "4.5",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 57,
+        lastMileTravel: 11.9,
+        serviceability: "SERVICEABLE_WITH_BANNER",
+        slaString: "57 mins",
+        lastMileTravelString: "11.9 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextOpenTimeMessage: "Currently not taking orders for this location",
+        nextCloseTime: "2023-08-06 23:50:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-c1dacc1c-dcd8-4cd5-b9d5-072491c5bb0b",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/nic-ice-creams-pushpakdham-kalyan-mumbai-169878",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+];
+
+const RestaurantCard = ({
+  name,
+  cuisines,
+  avgRatingString,
+  cloudinaryImageId,
+}) => {
+  return (
+    <div className="card">
+      <img
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+      />
+      <h2>{name}</h2>
+      <h3>{cuisines.join(",")}</h3>
+      <h4>{avgRatingString} rating</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="restaurantlist">
+      {/* <RestaurantCard restaurant={RestaurantList[0]} /> */}
+      {/* <RestaurantCard {...RestaurantList[0].info} /> */}
+      {/* <RestaurantCard {...RestaurantList[1].info} />
+      <RestaurantCard {...RestaurantList[2].info} />
+      <RestaurantCard {...RestaurantList[3].info} />
+      <RestaurantCard {...RestaurantList[4].info} />
+      <RestaurantCard {...RestaurantList[5].info} /> */}
+      {RestaurantList.map((restaurant) => {
+        return <RestaurantCard {...restaurant.info} key={restaurant.info.id} />;
+      })}
+    </div>
+  );
+};
+
+const Footer = () => {
+  return <h4>Footer</h4>;
+};
+
+const AppLayout = () => {
+  return (
+    <div>
+      <HeaderComponent />
+      <Body />
+      <Footer />
+    </div>
+  );
+};
+
+root.render(<AppLayout />);

d67b8ebef4d310c514b6befa3dd51795acc98ead
Hrithik Kedare <kedarehrithik@gmail.com>
2023-08-11 10:11:24 +0530

    test5

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 
 import HeaderComponent from "./components/Header";
 
-import Body from "./components/body";
+import Body from "./components/Body";
 
 
 import Footer from "./components/Footer";
